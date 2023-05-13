2b1039d068833430b5acdc46e0c7346d21b30d63
Christhian Rezende Vieira <christhian11rv@hotmail.com>
2023-05-12 23:38:43 -0300

    Test 3
    asdf

---
+++ Assets/Test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    int test = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        test = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
